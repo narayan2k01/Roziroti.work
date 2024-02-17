ca765177dbf6e91e4d4a6e61acd634676a3dcd2e
Narayan sharma <narayan2k01@gmail.com>
2024-02-17 13:38:46 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    $('#owl-carousel1').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
